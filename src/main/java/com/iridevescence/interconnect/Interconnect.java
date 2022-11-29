604945a8b882e6464b218dd7c376d01ecb3dfd03
PrismaticYT <45874270+RPMYT@users.noreply.github.com>
2022-11-29 21:39:31 +0000

    I said we maven now

--- src/main/java/com/iridevescence/interconnect/Interconnect.java
+++ src/main/java/com/iridevescence/interconnect/Interconnect.java
@@ -1,17 +1,32 @@
 package com.iridevescence.interconnect;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 
 public final class Interconnect extends JavaPlugin {
+    private BukkitAudiences adventure;
 
+    public @NonNull BukkitAudiences adventure() {
+      if(this.adventure == null) {
+        throw new IllegalStateException("Tried to access Adventure when the plugin was disabled!");
+      }
+      return this.adventure;
+    }
+  
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+      // Initialize an audiences instance for the plugin
+      this.adventure = BukkitAudiences.create(this);
+      // then do any other initialization
     }
-
+  
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+      if(this.adventure != null) {
+        this.adventure.close();
+        this.adventure = null;
+      }
     }
 }
