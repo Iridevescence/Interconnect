fd8707bb4c0c1f8a8f39abaf38ce8445594172b5
PrismaticYT <45874270+RPMYT@users.noreply.github.com>
2022-11-30 01:08:08 +0000

    S2I/I2S mapping utility & restriction registry

--- src/main/java/com/iridevescence/interconnect/api/skill/Registry.java
+++ src/main/java/com/iridevescence/interconnect/api/skill/Registry.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 
 public class Registry {
     private static final HashMap<Restriction.Type, Restriction> RESTRICTIONS = new HashMap<>();
+    private static final HashMap<String, Restriction> S2R_MAPPING = new HashMap<>();
 
     public static void addRestriction(Restriction restriction) {
         RESTRICTIONS.put(restriction.type(), restriction);
+        S2R_MAPPING.put(restriction.name(), restriction);
     }
 
     public static ArrayList<Restriction> getRestrictionsByType(Restriction.Type type) {
@@ -18,5 +20,9 @@ public class Registry {
             }
         });
         return list;
+    }
+
+    public static Restriction getRestrictionByName(String name) {
+        return S2R_MAPPING.get(name);
     }
 }
---
+++ src/main/java/com/iridevescence/interconnect/util/StringIndexUtil.java
@@ -0,0 +1,16 @@
+package com.iridevescence.interconnect.util;
+
+import java.util.HashMap;
+
+public class StringIndexUtil {
+    private static final HashMap<Integer, String> I2S = new HashMap<>();
+    private static final HashMap<String, Integer> S2I = new HashMap<>();
+    
+    public static String getStringAt(int index) {
+        return I2S.get(index);
+    }
+
+    public static int getIndexOf(String str) {
+        return S2I.get(str);
+    }
+}
